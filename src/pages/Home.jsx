b793cf333da40f4056ec04f1e1e3caef5200f73c
DvLeu <l21020400@veracruz.tecnm.mx>
2026-03-22 23:21:57 -0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () =>{
+    return <div className="min-h-screen container">
+        {/*Theme Toggle */}
+
+        {/*Background */}
+
+        {/*Navbar */}
+
+        {/*Main Content */}
+        
+        {/*Footer */}
+
+    </div>
+}
