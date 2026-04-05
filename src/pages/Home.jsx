ede06d5d992e24ce0740911d11162e0b5b7b1f37
Dvleu <L21020400@veracruz.tecnm.mx>
2026-04-05 15:33:41 -0600

    Added Vercel Analytics for more detailing on production, corrected ProjectSection Naming for Standar use.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,16 @@ import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
-import { ProjectSection } from "../components/ProjectsSection";
+import { ProjectSection } from "../components/ProjectSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Analytics from vercel */}
+      <Analytics/>
       {/*Background Effects */}
       <StarBackground />
       {/*Navbar */}
