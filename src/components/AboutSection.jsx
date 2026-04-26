a18ac246cd6365f783061464e7541cead4e7ab68
DvLeu <l21020400@veracruz.tecnm.mx>
2026-04-26 12:44:57 -0600

    New CV updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,8 +30,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="Dvleu-CV.pdf"
-                download="Dvleu-CV.pdf"
+                href="David_Leon_Salas_CV.pdf"
+                download="David_Leon_Salas_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
